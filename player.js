88bb5548b15d7453954005623fc24d37b5d117e1
Pratham Jain <2muchappy420@gmail.com>
2024-08-31 01:02:23 +0530

    Added Enemies

--- player.js
+++ player.js
@@ -1,18 +1,30 @@
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
+
 export class Player {
     constructor(game) {
         this.game = game;
         this.width = 100;
         this.height = 91.3;
         this.x = 0;
-        this.y = 0;
+        this.y = this.game.height - this.height - this.game.groundMargin;
         this.image = document.getElementById("player");
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame;
+        this.fps = 20;
+        this.frameInterval = 1000 / this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
         this.vy = 0; //vertical speed
         this.weight = 1;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
 
     update(input) {
+        this.currentState.handleInput(input);
         // hori movement
         this.x += this.speed;
         if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
@@ -22,19 +34,35 @@ export class Player {
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
 
         // vertical movement
-        if (input.includes("ArrowUp") && this.onGround()) this.vy -= 10;
         this.y += this.vy;
         if(!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+
+        //sprite animation
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrmae) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
+        // if (this.frameX > this.maxFrame) this.frameX++;
+        // else this.frameX = 0;
     }
 
     draw(context) {
         // context.fillStyle = 'red';
         // context.fillRect(this.x, this.y, this.width, this.height);
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 
     onGround() {
-        return this.y >= this.game.height - this.height;
+        return this.y >= this.game.height - this.height -this.game.groundMargin;
+    }
+
+    setState(state, speed) {
+        this.currentState = this.states[state];
+        this.game.speed = this.game.maxSpeed * speed;
+        this.currentState.enter();
     }
 }
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
+import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
 
 window.addEventListener("load", () => {
     const canvas  = document.getElementById("canvas1");
@@ -11,26 +13,56 @@ window.addEventListener("load", () => {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.groundMargin = 80;
+            this.speed = 0;
+            this.maxSpeed = 3;
+            this.background = new Background(this)
             this.player = new Player(this);
             this.input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
         }
 
-        update() {
-
+        update(deltaTime) {
+            this.background.update();
+            this.player.update(this.input.keys, deltaTime);
+            
+            // handle enemies
+            if (this.enemyTimer > this.enemyInterval) {
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+            })
         }
 
-        draw() {
+        draw(context) {
+            this.background.draw(context);
             this.player.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            })
+        }
+
+        addEnemy() {
+            this.enemies.push(new FlyingEnemy(this));
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
 
-    function animate() {
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.update();
+        game.update(deltaTime);
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 });
