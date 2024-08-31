2e0283b9dd5ab8eeb2c2edadece0e2260f3ab904
Pratham Jain <2muchappy420@gmail.com>
2024-08-31 16:39:50 +0530

    Added basic enemy movement

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ window.addEventListener("load", () => {
             }
             this.enemies.forEach(enemy => {
                 enemy.update(deltaTime);
+                if (enemy.markedForDeletion = true) {
+                    this.enemies.splice(this.enemies.indexOf(enemy), 1);
+                }
             })
         }
 
@@ -49,6 +52,8 @@ window.addEventListener("load", () => {
         }
 
         addEnemy() {
+            if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this)); 
             this.enemies.push(new FlyingEnemy(this));
         }
     }
